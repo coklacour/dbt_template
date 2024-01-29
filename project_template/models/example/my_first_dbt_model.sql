00b07b41592b1bcff8efcd321eb2da45b9130a2b
alluardj <alluardj@csvdc.qc.ca>
2024-01-29 13:22:06 -0500

    chore: remove comments from initial example scripts

--- project_template/models/example/my_first_dbt_model.sql
+++ project_template/models/example/my_first_dbt_model.sql
@@ -1,31 +1,18 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='table') }}
 
-with source_data as (
+WITH source_data AS (
 
-    select 1 as id
-    union all
-    select null as id
-    union all
-    select null as id
-    union all
-    select 2 as id
+    SELECT 1 AS id
+    UNION ALL
+    SELECT null AS id
+    UNION ALL
+    SELECT null AS id
+    UNION ALL
+    SELECT 2 AS id
 
 )
 
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+SELECT 
+    id
+    , id + 3 AS id2
+FROM source_data
